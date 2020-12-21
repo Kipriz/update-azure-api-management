bd4330b8080223dead65a8ff9c3bd35699ec2570
Andrey Volkov <andvolkov@gmail.com>
2020-12-21 16:34:34 +0300

    More logging

--- src/index.js
+++ src/index.js
@@ -18,25 +18,31 @@ async function run() {
         const format = core.getInput("format") || "openapi+json-link";
         const version = core.getInput("versionOfApiManagement") || "2020-06-01-preview";
 
-        core.info("Configuration has been read successfully.")
+        core.info("Configuration has been read successfully.");
+        core.info(`SubscriptionId: ${subscriptionId}`);
+        core.info(`resourceGroup: ${resourceGroup}`);
+        core.info(`serviceName: ${serviceName}`);
+        core.info(`apiName: ${apiName}`);
+        core.info(`apiPath: ${apiPath}`);
+        core.info(`linkToOpenApi: ${linkToOpenApi}`);
 
-        const link = `https://management.azure.com/subscriptions/${subscriptionId}/resourceGroups/${resourceGroup}/providers/Microsoft.ApiManagement/service/${serviceName}/apis/${apiName}?api-version=${version}`
+        const link = `https://management.azure.com/subscriptions/${subscriptionId}/resourceGroups/${resourceGroup}/providers/Microsoft.ApiManagement/service/${serviceName}/apis/${apiName}?api-version=${version}`;
         const data = {
             properties: {
                 path: apiPath,
                 format: format,
                 value: linkToOpenApi
             }
-        }
+        };
         const config = {
             headers: {
                 "Authorization": authorization,
                 "Content-type": "application/json"
             }
-        }
+        };
 
-        core.info("Start updating...")
-        const response = await axios.put(link, data, config)
+        core.info("Start updating...");
+        const response = await axios.put(link, data, config);
         core.info(`Done sending update request, response: ${response.status} ${response.statusText}`);
 
     } catch (error) {
